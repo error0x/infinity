e7b4862bcb6909c1f41d7a5fd0fc06e3ed98aee3
odium <tobi@stardrakkar.eu>
2022-06-04 18:23:47 +0200

    Generator test

---
+++ generator.php
@@ -0,0 +1,13 @@
+<?php
+$antagonist=array(
+    "red","
+    green",
+    "blue",
+    "yellow",
+    "brown"
+);
+$random_keys=array_rand($a,3);
+echo $a[$random_keys[0]]."<br>";
+echo $a[$random_keys[1]]."<br>";
+echo $a[$random_keys[2]];
+?>
